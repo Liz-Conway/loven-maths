346f440637ee2d5be3c49068c2164b998c38de9d
Liz-Conway <zil.conway@gmail.com>
2021-10-31 11:31:01 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Connected");
+function runGame() {
+}
+function checkAnswer() {
+}
+function calculateCorrectAnswer() {
+}
+function incrementScore(){
+}
+function incrementWrongAnswer(){
+}
+function displayAdditionQuestion (){
+}
+function displayMultiplyQuestions(){
+}
+function displaySubtractionQuestion(){
+}
